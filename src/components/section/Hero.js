6dd929eade9f9f93aa9b190b108a98168b04d233
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-11 15:26:40 +0700

    FE-HERO-v2 Added Hero section component

--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -1,8 +1,11 @@
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Box marginX={10} marginY="5">
+        <Text as="b" fontSize={"6xl"}>TUPO</Text>
+    </Box>
   )
 }
 
