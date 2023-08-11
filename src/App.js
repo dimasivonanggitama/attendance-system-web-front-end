b99cfb9a7d7e8acf3b043005bf1dc735214ceec7
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-11 18:02:06 +0700

    FE-ROUT-v2 Comment unused routes + Deleted unused import App.css component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import LandingPage from "./pages/LandingPage";
-import ForgotPassword from "./pages/ForgotPassword";
-import SuccessPage from "./pages/guest/SuccessPage";
-import ResetPasswordPage from "./pages/guest/ResetPasswordPage";
-import MainPage from "./pages/user/MainPage";
-import TestPage from "./pages/TestPage";
-import ProductPage from "./pages/user/ProductPage";
+import LandingPage from "./pages/guest/LandingPage";
+// import ForgotPassword from "./pages/ForgotPassword";
+// import SuccessPage from "./pages/guest/SuccessPage";
+// import ResetPasswordPage from "./pages/guest/ResetPasswordPage";
+// import MainPage from "./pages/user/MainPage";
+// import TestPage from "./pages/TestPage";
+// import ProductPage from "./pages/user/ProductPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
-        <Route path="/forgotPassword" element={<ForgotPassword/>} />
+        {/* <Route path="/forgotPassword" element={<ForgotPassword/>} />
         <Route path="/forgotPassword/emailSent" element={<SuccessPage purpose="email"/>} />
         <Route path="/forgotPassword/passwordReset" element={<SuccessPage purpose="reset"/>} />
         <Route path="/forgotPassword/resetPassword" element={<ResetPasswordPage/>} />
         <Route path="/main" element={<MainPage role="kasir"/>} />
         <Route path="/product" element={<ProductPage role="admin" sidebar="product"/>} />
-        <Route path="/test" element={<TestPage/>} />
+        <Route path="/test" element={<TestPage/>} /> */}
       </Routes>
     </Router>
   );
