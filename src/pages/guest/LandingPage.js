456e432427c9082f1930e62158b2c5f4b1356dca
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-11 15:30:17 +0700

    FE-HOME-v2 Added Landing Page components

--- src/pages/guest/LandingPage.js
+++ src/pages/guest/LandingPage.js
@@ -1,8 +1,21 @@
+import { Box, VStack } from '@chakra-ui/react'
 import React from 'react'
+import LoginForm from '../components/form/LoginForm'
+import Hero from '../components/section/Hero'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <Box
+      display='flex'
+      justifyContent={"center"}
+      height="calc(100vh)"
+      width='100%'
+    >
+      <Box flex={2} bgColor="lightgrey"><Hero/></Box>
+      <VStack flex={1} bgColor={"blackAlpha.100"} justifyContent={"center"}>
+        <LoginForm/>
+      </VStack>
+    </Box>
   )
 }
 
