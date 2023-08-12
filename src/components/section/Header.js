8fbd40b570f37b0a2ab6feff8feeaed495273170
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-12 19:45:30 +0700

    FE-HEAD-v2 Added Header components

--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -1,8 +1,18 @@
+import { Avatar, Badge, Box, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Box display={"flex"} justifyContent={"space-between"} height={"10vh"} boxShadow={'md'} bgColor={"blackAlpha.50"}>
+        <Text marginX={5} textAlign="center" as="b" fontSize="6xl">TUPO</Text>
+        <HStack margin={5} paddingX={3} borderRadius={15} boxShadow={"md"} bgColor={"white"} _hover={{background: "gray.200"}}>
+            <Avatar name="Dimas Ivon Anggitama" src="#" marginRight={5}/>
+            <Box>
+                <Text>Dimas Ivon Anggitama</Text>
+                <Badge colorScheme='red'>Admin</Badge>
+            </Box>
+        </HStack>
+    </Box>
   )
 }
 
