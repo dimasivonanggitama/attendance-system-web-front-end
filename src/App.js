386e1c1bf9c02e143625ff7e27e532a85978d30e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-12 20:09:11 +0700

    FE-ROUT-v3 Added User Management Page to route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/guest/LandingPage";
+import UserManagementPage from "./pages/admin/UserManagementPage";
 // import ForgotPassword from "./pages/ForgotPassword";
 // import SuccessPage from "./pages/guest/SuccessPage";
 // import ResetPasswordPage from "./pages/guest/ResetPasswordPage";
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage/>} />
+        <Route path="/userManagement" element={<UserManagementPage/>} />
         {/* <Route path="/forgotPassword" element={<ForgotPassword/>} />
         <Route path="/forgotPassword/emailSent" element={<SuccessPage purpose="email"/>} />
         <Route path="/forgotPassword/passwordReset" element={<SuccessPage purpose="reset"/>} />
