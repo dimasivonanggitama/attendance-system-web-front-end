c5ca58f5984ae24441047034c41d03787db37f10
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-12 21:08:59 +0700

    FE-USMA-v3 Added 'Add Employee' Button

--- src/pages/admin/UserManagementPage.js
+++ src/pages/admin/UserManagementPage.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Dashboard from '../user/Dashboard'
+import { Box, HStack, Table, TableCaption, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
+import { TbUserPlus } from 'react-icons/tb'
 
 const UserManagementPage = () => {
   return (
-    <Dashboard></Dashboard>
+    <Dashboard>
+      <Box>
+        <HStack borderRadius={15} boxShadow={"md"} padding={"3"} bgColor={"white"} _hover={{background: "gray.200"}} >
+          <TbUserPlus size={25}/>
+          <Text>Tambah Pegawai</Text>
+        </HStack>
+      </Box>
+    </Dashboard>
   )
 }
 
