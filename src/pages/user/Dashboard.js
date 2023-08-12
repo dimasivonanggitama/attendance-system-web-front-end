71f7bd03efc0a18ad30d73c0da4858fe8caefc0c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-12 19:53:58 +0700

    FE-DASH-v2 Added Dashboard template page component

--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Header from '../../components/section/Header'
+import Sidebar from '../../components/section/Sidebar'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   return (
-    <div>Dashboard</div>
+    <Box height={"90vh"}>
+      <Header />
+      <Box display={"flex"} flexDirection={"row"}>
+        <Sidebar role={props.role} sidebar={props.sidebar}/>
+        <Box display={"flex"} maxHeight={"90vh"} width="100%" bgColor={"lightgrey"} overflowY={"auto"}>
+          {props.children}
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
