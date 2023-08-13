a4a8ac8e2d42682c07a10c2b43b77ee14effab78
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-13 16:11:46 +0700

    FE-ROFO-v2 Added 1 input & 1 button in one line

--- src/components/form/RoleForm.js
+++ src/components/form/RoleForm.js
@@ -1,8 +1,15 @@
+import { Box, Button, HStack, Input } from '@chakra-ui/react'
 import React from 'react'
+import InputWithError from '../input/InputWithError'
 
 const RoleForm = () => {
   return (
-    <div>RoleForm</div>
+    <HStack>
+        <InputWithError margin={"0"} padding={"0"}>
+            <Input type="text" name="username" placeholder='Username' bgColor="white" borderColor={"grey"} color={"black"} />
+        </InputWithError>
+        <Button type="submit" colorScheme={"green"}>Tambah</Button>
+    </HStack>
   )
 }
 
