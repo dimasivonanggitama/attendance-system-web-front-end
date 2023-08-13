109d2cdead196aa7787450d86047b477b5fc7611
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-13 13:12:50 +0700

    FE-ROMA-v2 Added Dashboard & table component

--- src/pages/admin/RoleManagementPage.js
+++ src/pages/admin/RoleManagementPage.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import Dashboard from '../user/Dashboard'
+import { Box, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 
 const RoleManagementPage = () => {
-  return (
-    <div>RoleManagementPage</div>
-  )
+    return (
+        <Dashboard>
+            <Box>
+                <Box bgColor={"white"} borderRadius={15}>
+                    <TableContainer>
+                        <Table variant="simple">
+                            <Thead>
+                                <Tr>
+                                    <Th>To convert</Th>
+                                    <Th >into</Th>
+                                    <Th isNumeric>multiply by</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                <Tr>
+                                    <Td>inches</Td>
+                                    <Td>millimetres (mm)</Td>
+                                    <Td isNumeric>25.4</Td>
+                                </Tr>
+                                <Tr>
+                                    <Td>feet</Td>
+                                    <Td>centimetres (cm)</Td>
+                                    <Td isNumeric>30.48</Td>
+                                </Tr>
+                                <Tr>
+                                    <Td>yards</Td>
+                                    <Td>metres (m)</Td>
+                                    <Td isNumeric>0.91444</Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </Box>
+        </Dashboard>
+    )
 }
 
 export default RoleManagementPage
