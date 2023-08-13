ed56066bb7315fe6efa10dec830328be9d72aaae
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-13 11:24:06 +0700

    FE-MORE-v3 Added default text for primary button + Changed props color variable primary button

--- src/components/modal/ModalRegular.js
+++ src/components/modal/ModalRegular.js
@@ -2,23 +2,25 @@ import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, 
 import React from 'react'
 
 const ModalRegular = (props) => {
-  return (
-    <Modal isOpen={props.isOpen} onClose={props.onCloseX}>
-        <ModalOverlay />
-        <ModalContent>
-            <ModalHeader>{props.title}</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody textAlign={"center"}>{props.children}</ModalBody>
-            <ModalFooter>
-                {
-                    (props.secondaryButton)? <Button variant='ghost' marginRight={5} onClick={props.onClickSecondaryButton}>{props.secondaryButton}</Button>
-                    : <></>
-                }
-                <Button colorScheme={props.defaultButtonColor} mr={3} onClick={props.onClose}>{props.primaryButton}</Button>
-            </ModalFooter>
-        </ModalContent>
-    </Modal>
-  )
+    let primaryButtonText = "";
+    (props.primaryButton)? primaryButtonText = props.primaryButton : primaryButtonText = "Button";
+    return (
+        <Modal isOpen={props.isOpen} onClose={props.onCloseX}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>{props.title}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody textAlign={"center"}>{props.children}</ModalBody>
+                <ModalFooter>
+                    {
+                        (props.secondaryButton)? <Button variant='ghost' marginRight={5} onClick={props.onClickSecondaryButton}>{props.secondaryButton}</Button>
+                        : <></>
+                    }
+                    <Button colorScheme={props.primaryButtonColor} mr={3} onClick={props.onClose}>{primaryButtonText}</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
 }
 
 export default ModalRegular
