b9a18450de9699a860ca8f3fa3d525ad5dfd5f63
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-15 16:50:50 +0700

    FE-ERFO-v10 Added modal title preset

--- src/components/form/EditRoleForm.js
+++ src/components/form/EditRoleForm.js
@@ -8,6 +8,11 @@ import { Box, Button, Input, ModalBody, ModalFooter, ModalHeader, Text } from '@
 import { TbShieldCog } from 'react-icons/tb';
 
 const EditRoleForm = (props) => {
+    const modalRoleEditTitle = <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+        <TbShieldCog size={70}/>
+        <Text as={"b"} fontSize="2xl">Ubah Peran</Text>
+    </Box>;
+    
     const editRoleSchema = useFormik({
         initialValues: {
             roleNameEdit: props.selectedRole.role_name
@@ -31,7 +36,7 @@ const EditRoleForm = (props) => {
     });
     return (
         <form onSubmit={editRoleSchema.handleSubmit}>
-            <ModalHeader>{props.title}</ModalHeader>
+            <ModalHeader>{modalRoleEditTitle}</ModalHeader>
                 <ModalBody textAlign={"center"}>
                     <InputWithError margin={"0"} padding={"1"} errors={editRoleSchema.errors.roleNameEdit} touched={editRoleSchema.touched.roleNameEdit}>
                         <Input type="text" name="roleNameEdit" placeholder='Ubah peran' bgColor="white" borderColor={"grey"} color={"black"} value={editRoleSchema.values.roleNameEdit} onChange={editRoleSchema.handleChange}/>
