10ce11fa8bc555211beaf7d3d09653d87f09aa6b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-15 11:56:36 +0700

    FE-ERFO-v2 Added Formik Yup + Axios + props for selected role

--- src/components/form/EditRoleForm.js
+++ src/components/form/EditRoleForm.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import axios from 'axios';
+import React from 'react';
 
-const EditRoleForm = () => {
-  return (
-    <div>EditRoleForm</div>
-  )
+const EditRoleForm = (props) => {
+    const editRoleSchema = useFormik({
+        initialValues: {
+            roleNameEdit: props.selectedRole
+        },
+        validationSchema: Yup.object({
+            roleNameEdit: Yup.string()
+                .matches(/^[a-zA-Z]+$/, "Hanya huruf yang diperbolehkan!")
+                .required("Peran tidak boleh kosong!"),
+        }),
+        onSubmit: async values => {
+            await axios.post("http://localhost:8000/api/admin/role/test", {
+                roleNameEdit: values.roleNameEdit
+            }).then(resp => {
+                setIsModalEditOpened(false);
+                fetchData();
+            }).catch(error => {
+                console.log(error.response.data.error);
+                alert(error.response.data.message);
+            });
+        }
+    });
+    return (
+        <div>EditRoleForm</div>
+    )
 }
 
 export default EditRoleForm
